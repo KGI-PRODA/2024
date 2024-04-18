d72bf8c924ce01b181f7a6ca298a654188043679
JanDedič <jan.dedic158@gmail.com>
2024-04-18 12:00:07 +0200

    .

--- cviceni9/open.py
+++ cviceni9/open.py
@@ -6,17 +6,19 @@ for root,dirs,filenames in os.walk(os.getcwd()):
         if (filename.endswith(".png")):
             path = (os.path.join(root, filename)) 
             files.append(path)
-print(files)
-#os.system("C:/GIMP/GIMP2/bin/gimp-2.10 " + " ".join(files))
+input_file = "spustit_gimp.bat"
+with open(input_file, "w") as file:
+    file.write(f"C:/GIMP/GIMP2/bin/gimp-2.10 {' '.join(files)}")
+os.system("spustit_gimp.bat")
 
 """
 Skript, který otevírá snímky do GIMPU
 
 Vytvořím si prázdný seznam, následně pomocí for cyklu iteruju přes adresářovou strukturu a příkazem os.walk ji procházím
 a os.getcwd zapíše cestu k souboru. další for loop se už konkrétně kouká na snímky.
-Pokud soubor končí .png, tak jej přidá do seznamu s kompletní cestou. Následně se spustí GIMP, který se načítá s cestami uloženými v seznamu.
+Pokud soubor končí .png, tak jej přidá do seznamu s kompletní cestou.
+
+Kvůli omezenosti CMD 8000 znaků se musí vytvořit otevírací soubor. Dálší řádek otevře soubor spustit_gimp.bat pro zápis, do kterého
+zapisuje cestu ke spuštění gimpu + k němu přidá .png ze seznamu a na závěr spustí .bat
 
 """
-
-
-
